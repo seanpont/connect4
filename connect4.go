08fb6160de761b8f9ff6fd02b73b270a92aaa76b
Sean Pont <sean@cotap.com>
2013-12-20 08:40:05 -0800

    looks much nicer

--- connect4.go
+++ connect4.go
@@ -7,10 +7,13 @@ import (
 func main() {
 	fmt.Println("Connect4!")
 
+	p1 := promptToken(1)
+	p2 := promptToken(2)
+
 	b := NewBoard()
 	won := false
 	for !won {
-		fmt.Println(b)
+		printBoard(b, p1, p2)
 		columnIndex := 0
 		fmt.Scanf("%d", &columnIndex)
 		w, err := b.Play(columnIndex)
@@ -19,6 +22,54 @@ func main() {
 		}
 		won = w
 	}
-	fmt.Println("Congradulations player", b.turn)
+	printBoard(b, p1, p2)
+	fmt.Printf("Congratulations player %v!\n", b.turn)
 }
+
+func promptToken(player int) string {
+	fmt.Printf("Player %d: enter your token: ", player)
+	var p1 string
+	_, err := fmt.Scanln(&p1)
+	if (err != nil) {
+		panic("Could not read token")
+	}
+	if len(p1) > 1 {
+		fmt.Println("Token must be one character")
+		return promptToken(player)
+	}
+	return p1
+}
+
+func printBoard(b *Board, p1 string, p2 string) {
+	s := "\n"
+	for rowIndex := ROWS-1; rowIndex >= 0; rowIndex-- {
+		s += "|"
+		for _, val := range(b.board[rowIndex]) {
+			s += " "
+			if val == byte(1) {
+				s += p1
+			} else if val == byte(2) {
+				s += p2
+			} else {
+				s += " "
+			}
+		}
+		s += " |\n"
+	}
+	s += "|---------------|\n| 1 2 3 4 5 6 7 |"
+	if b.turn == byte(2) {
+		p1 = p2
+	}
+	s += fmt.Sprintf("\nPlayer %v(%v): ", b.turn, p1)
+	fmt.Print(s)
+}
+
+
+
+
+
+
+
+
+
 
